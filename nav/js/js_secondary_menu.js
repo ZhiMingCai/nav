61f6648ba7c7998cbc7d97ab6cf37e6be5d41993
caizhiming <506742195@qq.com>
2017-07-30 07:08:09 -0500

    secondary_menu
    使用css版本和原生js版本写的二级导航栏

---
+++ nav/js/js_secondary_menu.js
@@ -0,0 +1,9 @@
+function showsubmenu(li){
+	var submenu = li.getElementsByTagName("ul")[0];
+	submenu.style.display = "block";
+}
+
+function hide(li){
+	var submenu = li.getElementsByTagName("ul")[0];
+	submenu.style.display = "none";
+}
